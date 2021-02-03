145deb5d8f6d58307493c210c5fa33c4052881fd
Omar <dev0mar137@outlook.com>
2021-02-03 20:07:57 -0300

    add js

---
+++ js/logica.js
@@ -0,0 +1,6 @@
+const screen = document.getElementById("display"),
+      btns = document.getElementById("button-container");
+btns.addEventListener("click", load, false);
+function load (){
+    console.log("Op!!")
+}
