14f5a71d81daab5d10e172a26e286520117ee94c
Omar <dev0mar137@outlook.com>
2021-02-04 11:15:37 -0300

    Va queriendo

--- js/logica.js
+++ js/logica.js
@@ -1,6 +1,31 @@
-const screen = document.getElementById("display"),
+const screen = document.querySelector('#display'),
       btns = document.getElementById("button-container");
-btns.addEventListener("click", load, false);
-function load (){
-    console.log("Op!!")
+let data;
+const getData = (ref) => {
+    let value = ref.value;
+    data = data == undefined ? value : parseInt(data += value);
+    screen.value += value;
+    if (parseInt(value) || parseInt(value) == 0) {
+        return parseInt(data);
+    } else {
+    console.log(value);
+        if (value === "c"){
+            clear();
+        } else if (value === "+") {
+            clear();
+            console.log("Sumamos");
+        } else if (value === "-") {
+            console.log("Restamos");
+        } else if (value === "*") {
+            console.log("Multi");
+        } else if (value === "/") {
+            console.log("Dividimos");
+        }
+    }
+}
+const clear = () => {
+    screen.value = '';
+}
+const sumar = (a, b) => {
+    return a + b;
 }
