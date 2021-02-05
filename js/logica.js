00d434114ac1140a7d952b60741e33cebb3ab70b
Omar <dev0mar137@outlook.com>
2021-02-05 20:40:53 -0300

    Calculador funcianal con excepciones

--- js/logica.js
+++ js/logica.js
@@ -1,11 +1,11 @@
 const screen = document.querySelector('#display'),
       btns = document.getElementById("button-container");
-let data = null, x = 0, opc;
+let data = null, x = null, opc = '=';
 const getData = (ref) => {
     let value = ref.value;
     // data = data == undefined ? value : console.log(parseInt(data += value));
-    screen.value += value;
     if (parseInt(value) || value == "0") {
+        screen.value += value;
         data = data == null ? value : parseInt(data += value);
     } else {
         if (value === "c"){
@@ -18,19 +18,24 @@ const getData = (ref) => {
             opc = value;
         } else if (value === "-") {
             clear();
-            x = data;
+            x = x != null && x != 0 ? x -= data : x = data;
             data = 0;
             opc = value;
         } else if (value === "*") {
             clear();
-            x = data;
+            x = x != null ? x *= data : x = data;
             data = 0;
             opc = value;
         } else if (value === "/") {
-            clear();
-            x = data;
-            data = 0;
-            opc = value;
+            if (data == 0) {
+                clear();
+                screen.value = 'Error'
+            } else {
+                clear();
+                x = x != null && x != 0 ? x /= data : x = data;
+                data = 0;
+                opc = value;
+            }
         } else if (value === "=") {
             clear();
             switch (opc) {
@@ -38,21 +43,25 @@ const getData = (ref) => {
                     screen.value = sumar(x, data);
                     data = 0;
                     x = 0;
+                    opc = '=';
                     break;
                 case '-':
                     screen.value = restar(x, data);
                     data = 0;
                     x = 0;
+                    opc = '=';
                     break;
                 case '*':
                     screen.value = multiplicar(x, data);
                     data = 0;
                     x = 0;
+                    opc = '=';
                     break;
                 case '/':
                     screen.value = dividir(x, data);
                     data = 0;
                     x = 0;
+                    opc = '=';
                     break;
                 default:
                     console.log("Nos fui"); 
